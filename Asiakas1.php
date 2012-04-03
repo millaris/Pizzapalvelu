6f8e6706401a0f007ddd1f26e3d4afb1d3f4fc0c
Milla Ristiluoma <milla.ristiluoma@helsinki.fi>
2012-04-03 15:33:23 +0300

    Asiakaslomake

---
+++ Asiakas1.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="Asiakas.php" method="post">
+            Nimi <input type="text" name="nimi" />
+            Puhelinnumero <input type="text" name="puhelinnumero" />
+            <input type="submit" value="Lisää" />
+         </form>
+    </body>
+</html>
