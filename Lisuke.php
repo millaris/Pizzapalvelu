1060548f128b762453e30d4c51b33d79e2e13b90
Tytti Järvinen <Tytti Jrvinen@192.168.1.35>
2012-04-14 17:15:32 +0300

    Lisukelista ja listan päivitys

---
+++ Lisuke.php
@@ -0,0 +1,28 @@
+<?php
+require_once "tietokanta.php";
+
+$yhteys = getTietokanta();
+$id = $_GET["id"];
+$lisuke = NULL;
+if ($id != NULL)
+{
+$kysely = $yhteys->prepare("select * from lisuke where lisukeid = $id");
+$kysely->execute();
+
+$lisuke = $kysely->fetch();
+}
+?>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+            <form action="Lisukelista.php" method="post">
+            <input type="hidden" name="id" value="<?php echo $lisuke['lisukeid'] ?>" />
+            Nimi <input type="text" name="nimi" value="<?php echo $lisuke['nimi'] ?>" />
+            Hinta<input type="integer" name="hinta" value="<?php echo $lisuke['hinta'] ?>" />
+            <input type="submit" value="Lisää" />
+         </form>
+    </body>
+</html>
