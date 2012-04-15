61fdee6c6d15bf96a33019db9f3ba4dfdc027d6f
Tytti Järvinen <Tytti Jrvinen@192.168.1.35>
2012-04-15 16:28:50 +0300

    muokkausta

--- Lisuke.php
+++ Lisuke.php
@@ -17,14 +17,25 @@ $lisuke = $kysely->fetch();
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Lisukkeiden lisäys ja muokkaus</title>
     </head>
     <body>
+        <h1>Lisukkeiden lisäys ja muokkaus</h1>
+        <table>
             <form action="Lisukelista.php" method="post">
             <input type="hidden" name="id" value="<?php echo $lisuke['lisukeid'] ?>" />
-            Nimi <input type="text" name="nimi" value="<?php echo $lisuke['nimi'] ?>" />
-            Hinta<input type="double" name="hinta" value="<?php echo $lisuke['hinta'] ?>" />
-            <input type="submit" value="Lisää" />
+            <tr>
+            <td><?php echo Nimi ?> </td> 
+            <td> <input type="text" name="nimi" value="<?php echo $lisuke['nimi'] ?>" </td>
+            </tr>
+            <tr>
+            <td><?php echo Hinta ?> </td>
+            <td> <input type="double" name="hinta" value="<?php echo $lisuke['hinta'] ?>" </td>
+            </tr>
+            <tr>
+            <td> <input type="submit" value="Tallenna" </td>
+            </tr>
          </form>
+        </table>
     </body>
 </html>
--- Tuote.php
+++ Tuote.php
@@ -15,16 +15,32 @@ $tuote = $kysely->fetch();
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Tuotteiden lisäys ja muokkaus</title>
     </head>
     <body>
+        <h1>Tuotteiden lisäys ja muokkaus</h1>
+        <table>
             <form action="Tuotelista.php" method="post">
             <input type="hidden" name="id" value="<?php echo $tuote['tuoteid'] ?>" />
-            Nimi <input type="text" name="nimi" value="<?php echo $tuote['nimi'] ?>" />
-            Kuva<input type="text" name="kuva" value="<?php echo $tuote['kuva'] ?>" />
-            Tekstikuvaus<input type="text" name="tekstikuvaus" value="<?php echo $tuote['tekstikuvaus'] ?>" />
-            Tyyppi<input type="text" name="tyyppi" value="<?php echo $tuote['tyyppi'] ?>" />
-            <input type="submit" value="Lisää" />
+            <tr>
+            <td><?php echo Nimi ?> </td> 
+            <td> <input type="text" name="nimi" value="<?php echo $tuote['nimi'] ?>" </td>
+            </tr>
+            <tr>
+            <td><?php echo Kuva ?> </td> 
+           <td> <input type="text" name="kuva" value="<?php echo $tuote['kuva'] ?>" </td>
+           </tr>
+            <tr>
+            <td><?php echo  Tekstikuvaus?> </td> 
+           <td> <input type="text" name="tekstikuvaus" value="<?php echo $tuote['tekstikuvaus'] ?>" </td>
+             </tr>
+            <tr>
+           <td><?php echo Tyyppi ?> </td> 
+            <td> <input type="text" name="tyyppi" value="<?php echo $tuote['tyyppi'] ?>" </td>
+            <tr>
+           <td> <input type="submit" value="Tallenna" </td>
+            </tr>
          </form>
+            </table>
     </body>
 </html>
