028a7b6be7f5da72e86c3634607d811e667b5ae3
Tytti Järvinen <Tytti Jrvinen@192.168.1.35>
2012-05-02 12:38:10 +0300

    muokkaus

---
+++ KirjauduitUlos.php
@@ -0,0 +1,13 @@
+
+<h1>Ulos kirjautuminen </h1>
+
+<?php
+
+
+require_once "tietokanta.php";
+
+$yhteys = getTietokanta();
+echo 'Olet nyt kirjautunut ulos. Kiitos käynnistä ja tervetuloa uudestaan!'
+?>
+
+<p><a href="Etusivu.php">Etusivulle</a> </p>
